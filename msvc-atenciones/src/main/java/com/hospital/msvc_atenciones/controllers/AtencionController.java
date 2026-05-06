2a5a17b20eba40f7244b14dc39c0dab4315f3f6e
Cesar Carrasco <ccarrascocarre@gmail.com>
2026-05-06 10:34:33 -0400

    Se genera la implementacion de controller atencion

---
+++ msvc-atenciones/src/main/java/com/hospital/msvc_atenciones/controllers/AtencionController.java
@@ -0,0 +1,52 @@
+package com.hospital.msvc_atenciones.controllers;
+
+import com.hospital.msvc_atenciones.models.Atencion;
+import com.hospital.msvc_atenciones.services.AtencionService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/atenciones")
+@Validated
+public class AtencionController {
+
+    @Autowired
+    private AtencionService atencionService;
+
+    @GetMapping
+    public ResponseEntity<List<Atencion>> findAll(){
+        return ResponseEntity.ok(atencionService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Atencion> findById(@PathVariable Long id){
+        return ResponseEntity.ok(atencionService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Atencion> save(@Valid @RequestBody Atencion atencion){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(atencionService.save(atencion));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Atencion> update(@PathVariable Long id, @Valid @RequestBody Atencion atencion){
+        return ResponseEntity.ok(atencionService.updateById(atencion, id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Atencion> delete(@PathVariable Long id){
+        atencionService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+}
